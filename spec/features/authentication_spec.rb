e74059bc8f6a46b2154ec472fdd0b7d472247021
Tom Sabin <tom.sabin@unboxed.co>
2018-07-16 11:37:01 +0100

    Search for a work order by reference

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Authentication' do
     click_link 'Sign in with Microsoft'
 
     expect(page).to have_current_path('/')
-    expect(page).to have_content 'You are logged in as Mike Ross'
-    expect(page).to have_link 'logout', href: %r{login.microsoftonline.com/.*/oauth2/logout\?post_logout_redirect_uri=.*logout}
+    expect(page).to have_content 'You have logged in as Mike Ross'
+    expect(page).to have_link 'Sign out', href: %r{login.microsoftonline.com/.*/oauth2/logout\?post_logout_redirect_uri=.*logout}
   end
 end
