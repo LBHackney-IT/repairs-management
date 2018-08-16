099de1b51fd15ded90d2a312e76a2bf963997659
Ben <benbaumann95@hotmail.co.uk>
2018-08-16 16:37:20 +0100

    Update API response to match notes endpoint

--- spec/support/helpers/hackney_repairs_request_stubs.rb
+++ spec/support/helpers/hackney_repairs_request_stubs.rb
@@ -283,14 +283,14 @@ module Helpers
     def work_order_notes_payload
       [
         {
-          "text": "Tenant called to confirm appointment",
-          "loggedAt": "2018-08-23T10:12:56+01:00",
-          "loggedBy": "MOSHEA"
+          "noteText": "Tenant called to confirm appointment",
+          "nDate": "2018-08-23T10:12:56+01:00",
+          "userID": "MOSHEA"
         },
         {
-          "text": "Further works required; Tiler required to renew splash back and reseal bath",
-          "loggedAt": "2018-09-02T11:32:14+01:00",
-          "loggedBy": "Servitor"
+          "noteText": "Further works required; Tiler required to renew splash back and reseal bath",
+          "nDate": "2018-09-02T11:32:14+01:00",
+          "userID": "Servitor"
         }
       ]
     end
