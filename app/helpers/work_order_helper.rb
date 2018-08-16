a43ff48595a79241c7926dde7c583f04497bee37
Tom Sabin <tom.sabin@unboxed.co>
2018-08-16 12:50:48 +0100

    Include appointment duration to clarify when target date is overdue

--- app/helpers/work_order_helper.rb
+++ app/helpers/work_order_helper.rb
@@ -1,5 +1,5 @@
 module WorkOrderHelper
   def out_of_target?(work_order, appointment)
-    work_order.date_due < appointment.visit_prop_end
+    work_order.date_due < appointment.visit_prop_appointment
   end
 end
