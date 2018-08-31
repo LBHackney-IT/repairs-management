b41c492ef3bbf46e17bf2d9334fb4ea9a92320ca
Ben Wong <ben.wong@unboxedconsulting.com>
2018-08-31 11:25:56 +0100

    use env to allow google auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,7 +8,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  if Rails.env.development?
+  if ENV['GOOGLE_AUTH'] == "true"
     creds = Rails.application.credentials.google_auth_unboxed
                  .slice(:client_id, :client_secret)
     provider :google_oauth2, *creds.values
