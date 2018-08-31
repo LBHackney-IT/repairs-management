203eb6261d2c74b8921124d97b87d17deb62927a
Ben Wong <ben.wong@unboxedconsulting.com>
2018-08-31 12:34:27 +0100

    update auth helpers to use google_auth env

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 
   def omniauth_login_link
-    if Rails.env.development?
+    if google_auth?
       link_to image_tag('btn_google_signin_light_normal_web.svg', alt: 'Sign in with Google'),
                         '/auth/google_oauth2'
     else
@@ -14,12 +14,16 @@ module ApplicationHelper
   end
 
   def omniauth_logout_url
-    if Rails.env.development?
+    if google_auth?
       logout_path
     else
       application_id = ENV['AAD_CLIENT_ID']
       callback_url = url_encode(logout_url)
       "https://login.microsoftonline.com/#{application_id}/oauth2/logout?post_logout_redirect_uri=#{callback_url}"
     end
+  end
+
+  def google_auth?
+    ENV['GOOGLE_AUTH'] == "true"
   end
 end
