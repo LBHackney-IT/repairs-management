aac23331092ce402c119195bd1cc7e276287029b
Tom Sabin <tom.sabin@unboxed.co>
2018-09-05 10:14:00 +0100

    Replace JS included by sprockets by webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,15 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from 'activestorage';
+import ActionCable from 'actioncable'
+import GOVUK from 'govuk-frontend';
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+GOVUK.initAll();
+
+var cable = ActionCable.createConsumer();
