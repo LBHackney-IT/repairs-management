d7c05b645334b5c001c6c4da730e2f3568f850f4
Piotr Jaworski <piotrek.jaw@gmail.com>
2018-09-10 16:02:33 +0100

    Added specs

--- spec/apis/hackney_api/repairs_client_spec.rb
+++ spec/apis/hackney_api/repairs_client_spec.rb
@@ -181,6 +181,41 @@ describe HackneyAPI::RepairsClient do
     end
   end
 
+  describe '#get_work_order_appointments_latest' do
+    subject { api_client.get_work_order_appointments_latest(reference) }
+
+    context 'successfull response' do
+      before { stub_request(:get, "#{base_url}/v1/work_orders/#{reference}/appointments/latest").to_return(body: empty_response_body.to_json) }
+
+      it 'returns successful response body' do
+        expect(subject).to eq(empty_response_body)
+      end
+    end
+
+    context 'not found error' do
+      before { stub_request(:get, "#{base_url}/v1/work_orders/#{reference}/appointments/latest").to_return(status: 404) }
+
+      it 'raises RecordNotFoundError error' do
+        expect { subject }.to raise_error(described_class::RecordNotFoundError)
+      end
+    end
+
+    context 'API general error' do
+      let(:response_body) do
+        {
+          "developerMessage" => "Exception of type 'HackneyRepairs.Actions.RepairsServiceException' was thrown.",
+          "userMessage" => "We had some problems processing your request"
+        }
+      end
+
+      before { stub_request(:get, "#{base_url}/v1/work_orders/#{reference}/appointments/latest").to_return(status: 500, body: response_body.to_json) }
+
+      it 'raises ApiError error' do
+        expect { subject }.to raise_error(described_class::ApiError).with_message("v1/work_orders/#{reference}/appointments/latest, 500, #{response_body}")
+      end
+    end
+  end
+
   describe '#get_work_order_notes' do
     subject { api_client.get_work_order_notes(reference) }
 
--- spec/models/hackney/appointment_spec.rb
+++ spec/models/hackney/appointment_spec.rb
@@ -1,16 +1,67 @@
 require 'rails_helper'
 
-describe Hackney::Appointment, '.build' do
+describe Hackney::Appointment do
   include Helpers::HackneyRepairsRequestStubs
 
-  it 'builds when the API response contains all fields' do
-    model = described_class.build(
-      "beginDate" => "2018-05-30T08:00:00",
-      "endDate" => "2018-05-30T12:00:00",
-      "sourceSystem" => "DRS"
-    )
+  describe '.build' do
+    let(:attributes) do
+      {
+        'id'=> '01551945',
+        'status'=> 'completed',
+        'assignedWorker'=> '(PLM) Tom Sabin Unboxed',
+        'phonenumber'=> '+44 mobile',
+        'priority'=> 'standard',
+        'sourceSystem'=> 'DRS',
+        'comment'=> 'FIRST',
+        'creationDate'=> '2018-05-31T15:05:51',
+        'beginDate'=> '2018-05-31T08:00:00',
+        'endDate'=> '2018-05-31T16:15:00'
+      }
+    end
 
-    expect(model).to be_a(Hackney::Appointment)
-    expect(model.end_date).to eq(DateTime.new(2018, 05, 30, 12, 00, 00))
+    subject { described_class.build(attributes) }
+
+    it 'builds when the API response contains all fields' do
+      expect(subject).to be_a(Hackney::Appointment)
+      expect(subject.end_date).to eq(DateTime.new(2018, 05, 31, 16, 15, 00))
+      expect(subject.begin_date).to eq(DateTime.new(2018, 05, 31, 8, 00, 00))
+      expect(subject.reported_on).to eq(DateTime.new(2018, 05, 31, 15, 5, 51))
+      expect(subject.comment).to eq('FIRST')
+      expect(subject.phone_number).to eq('+44 mobile')
+      expect(subject.assigned_worker).to eq('(PLM) Tom Sabin Unboxed')
+      expect(subject.priority).to eq('standard')
+      expect(subject.status).to eq('completed')
+    end
+  end
+
+  describe '.latest_for_work_order' do
+    let(:reference) { 'ref' }
+    let(:response) { {} }
+    let(:api_client_double) { instance_double(HackneyAPI::RepairsClient, get_work_order_appointments_latest: response) }
+    let(:appointment_double) { instance_double(described_class) }
+
+    subject { described_class.latest_for_work_order(reference) }
+
+    before do
+      allow(HackneyAPI::RepairsClient).to receive(:new).and_return(api_client_double)
+    end
+
+    context 'successful API response - latest appointment exists' do
+      it 'calls API client with a reference number and builds an instance of a class with valid attributes' do
+        expect(api_client_double).to receive(:get_work_order_appointments_latest).with(reference)
+        expect(described_class).to receive(:build).with(response).and_return(appointment_double)
+        expect(subject).to eq(appointment_double)
+      end
+    end
+
+    context 'not found API response - latest appointment does not exist' do
+      before do
+        allow(api_client_double).to receive(:get_work_order_appointments_latest).with(reference).and_raise(HackneyAPI::RepairsClient::RecordNotFoundError)
+      end
+
+      it 'calls API client with a reference nunmber and returns nil' do
+        expect(subject).to eq(nil)
+      end
+    end
   end
 end
