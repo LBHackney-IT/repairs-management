7c7fbc75b4d558180c57bc737999923fbb96b738
Celia Collins <celiadcollins@gmail.com>
2018-10-19 12:38:10 +0100

    Further config for Barnes

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,13 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+# require 'barnes'
+
+# before_fork do
+#   # worker specific setup
+#   Barnes.start
+# end
+
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
