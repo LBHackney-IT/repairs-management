d014b8cded539794a84ad9c3cebefc0b1c563283
Ben Wong <ben.wong@unboxedconsulting.com>
2018-10-29 14:50:40 +0000

    remove duplicates and self from related work orders

--- app/models/graph/work_order.rb
+++ app/models/graph/work_order.rb
@@ -13,7 +13,10 @@ class Graph::WorkOrder
   has_many :both, :cited_work_orders, rel_class: 'Graph::Citation'
 
   def related
-    query_as(:s).match("(s)-[*..#{MAX_RELATIONS} {extra: false}]-(p)").pluck(:p).to_a
+    query_as(:s)
+      .match("(s)-[*..#{MAX_RELATIONS} {extra: false}]-(p)")
+      .pluck(:p)
+      .to_a.uniq - [self]
   end
 
   def citations_for(work_order)
