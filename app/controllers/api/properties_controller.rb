f6de3a4b53576681b14a3f07d1ef9159d3330d88
Ben Wong <ben.wong@unboxedconsulting.com>
2018-11-07 17:29:26 +0000

    Pre fetch addresses for possibly related work orders to avoid fetching duplicate properties

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -2,6 +2,7 @@ module Api
   class PropertiesController < Api::ApiController
     def possibly_related_work_orders
       @property = Hackney::Property.find(reference)
+      @address_map = build_address_map(@property)
     end
 
     def repairs_history
@@ -12,6 +13,15 @@ module Api
 
     def reference
       params[:ref]
+    end
+
+    # Pre fetch addresses for all the work orders rather than delegate down to
+    # the work order to avoid fetching the property multiple times
+    def build_address_map(property)
+      property.work_orders_plumbing_from_block_and_last_two_weeks
+              .each_with_object({ property.reference => property.address }) do |work_order, map|
+        map[work_order.prop_ref] ||= work_order.property.address
+      end
     end
   end
 end
