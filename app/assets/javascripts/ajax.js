547660d3a623c8ae0c83158b3e4ffdf9c405a9d7
Ben <benbaumann95@hotmail.co.uk>
2018-11-09 11:57:21 +0000

    update front end stylistic and logic changes

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -19,12 +19,41 @@ function handleAjaxResponse(endpoint, ajaxTab) {
   request.send();
 }
 
-function ajaxRepairsHistoryFiveYears(endpoint) {
+function ajaxRepairsHistoryFiveYears(endpoint, ajaxTab) {
   var loadBtn = document.getElementById('load-repair-history-btn');
-  loadBtn.classList.add('ajax-loading')
+  var loadingText = document.createElement('p');
+  loadingText.innerHTML = 'Loading repairs history'
 
-  var tab = document.getElementById('repair-history-tab');
-  handleAjaxResponse(endpoint, tab);
+  loadBtn.parentNode.replaceChild(loadingText, loadBtn);
+  loadingText.classList.add('ajax-loading', 'govuk-caption-m');
 
-  loadBtn.parentNode.removeChild(loadBtn);
+  var ajaxTab = document.getElementById('repair-history-tab');
+
+  var request = new XMLHttpRequest();
+  var errorHandler = function() {
+    ajaxTab.innerHTML = 'There was a problem with an API while fetching data.'
+  };
+
+  request.open('GET', endpoint, true);
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        ajaxTab.innerHTML = request.response;
+
+        var repairHistoryYearsInfo = document.querySelector('.repair-history-years-info')
+        if (repairHistoryYearsInfo) {
+          repairHistoryYearsInfo.innerHTML = 'Repairs history is showing jobs raised in the last <strong>5 years</strong>.'
+        } else {
+          document.querySelector('.repair-history-no-work-orders').innerHTML = 'There are no work orders within the last <strong>5 years</strong>.'
+        }
+
+        document.querySelector('.load-repairs-history').classList.add('hidden');
+      } else {
+        errorHandler();
+      }
+    }
+  };
+
+  request.onerror = errorHandler;
+  request.send();
 }
