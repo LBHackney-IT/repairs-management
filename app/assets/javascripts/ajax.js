11ff1eb1a341a8e5aa2786cd36c48254d6f28e35
Ben <benbaumann95@hotmail.co.uk>
2018-11-12 10:59:24 +0000

    update front end style and refactor js

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -19,14 +19,8 @@ function handleAjaxResponse(endpoint, ajaxTab) {
   request.send();
 }
 
-function ajaxRepairsHistoryFiveYears(endpoint, ajaxTab) {
-  var loadBtn = document.getElementById('load-repair-history-btn');
-  var loadingText = document.createElement('p');
-  document.querySelector('.repair-history-years-info').classList.add('hidden')
-  loadingText.innerHTML = 'Loading repairs history'
-
-  loadBtn.parentNode.replaceChild(loadingText, loadBtn);
-  loadingText.classList.add('ajax-loading', 'govuk-caption-m');
+function handleAjaxRepairsHistoryFiveYears(endpoint, ajaxTab) {
+  handleClickShowRepairsHistoryBtn();
 
   var ajaxTab = document.getElementById('repair-history-tab');
 
@@ -40,17 +34,7 @@ function ajaxRepairsHistoryFiveYears(endpoint, ajaxTab) {
     if (request.readyState === 4) {
       if (request.status === 200) {
         ajaxTab.innerHTML = request.response;
-
-        var repairHistoryYearsInfo = document.querySelector('.repair-history-years-info')
-        repairHistoryYearsInfo.classList.remove('hidden');
-
-        if (document.querySelector('.hackney-work-order-table')) {
-          repairHistoryYearsInfo.innerHTML = 'Repairs history is showing jobs raised in the last <strong>5 years</strong>.'
-        } else {
-          repairHistoryYearsInfo.innerHTML = 'There are no work orders within the last <strong>5 years</strong>.'
-        }
-
-        document.querySelector('.load-repairs-history').classList.add('hidden');
+        handleRepairHistoryYearsInfoText();
       } else {
         errorHandler();
       }
@@ -59,4 +43,28 @@ function ajaxRepairsHistoryFiveYears(endpoint, ajaxTab) {
 
   request.onerror = errorHandler;
   request.send();
+}
+
+function handleClickShowRepairsHistoryBtn() {
+  var loadBtn = document.getElementById('load-repair-history-btn');
+  var loadingText = document.createElement('p');
+
+  document.querySelector('.repair-history-years-info').classList.add('hidden')
+  loadingText.innerHTML = 'Loading repairs history'
+
+  loadBtn.parentNode.replaceChild(loadingText, loadBtn);
+  loadingText.classList.add('ajax-loading', 'govuk-caption-m');
+}
+
+function handleRepairHistoryYearsInfoText() {
+  var repairHistoryYearsInfo = document.querySelector('.repair-history-years-info')
+  repairHistoryYearsInfo.classList.remove('hidden');
+
+  if (document.querySelector('.hackney-work-order-table')) {
+    repairHistoryYearsInfo.innerHTML = 'Repairs history is showing jobs raised in the last <strong>5 years</strong>.'
+  } else {
+    repairHistoryYearsInfo.innerHTML = 'There are no work orders within the last <strong>5 years</strong>.'
+  }
+
+  document.querySelector('.load-repairs-history').classList.add('hidden');
 }
