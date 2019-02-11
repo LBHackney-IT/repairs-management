bf1437ab0e203c283b429111ba08ff4ee5d0c65b
Celia Collins <celiadcollins@gmail.com>
2019-02-11 15:53:27 +0000

    Quick fix for Notes feed job
    Hack round getting the last note id because Graph::Note.last was very slow, because it's doing a table scan. We've made it faster by only looking at records with an id greater than 9 million

--- spec/models/graph/note_spec.rb
+++ spec/models/graph/note_spec.rb
@@ -6,4 +6,17 @@ describe Graph::Note, :db_connection do
 
     expect(Graph::Note.first.id).to eq 1
   end
+
+  it 'gets the last note id' do
+    Graph::Note.create!(note_id: 8_999_999, source: 'test', logged_at: Time.current, work_order_reference: '0' * 8)
+    Graph::Note.create!(note_id: 9_000_001, source: 'test', logged_at: Time.current, work_order_reference: '0' * 8)
+
+    expect(Graph::Note.last_note_id).to eq 9_000_001
+  end
+
+  it 'returns the last note id when there are less than 9 million records' do
+    Graph::Note.create!(note_id: 8_999_999, source: 'test', logged_at: Time.current, work_order_reference: '0' * 8)
+
+    expect(Graph::Note.last_note_id).to eq 8_999_999
+  end
 end
