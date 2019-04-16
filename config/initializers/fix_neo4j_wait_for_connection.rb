4ad4b82004b871e31c123cff42a07ad6c1496b62
space pudim <rogi@skylittlesystem.org>
2019-04-16 17:20:28 +0100

    Monkeypatch neo4j to fix wait_for_connection.
    This fix the crash when neo4j takes too long to boot on Heroku review
    apps.
    
    See https://github.com/neo4jrb/neo4j/pull/1540

---
+++ config/initializers/fix_neo4j_wait_for_connection.rb
@@ -0,0 +1,29 @@
+module Neo4j
+  class SessionManager
+    class << self
+      def open_neo4j_session(type, url_or_path, wait_for_connection = false, options = {})
+        enable_unlimited_strength_crypto! if java_platform? && session_type_is_embedded?(type)
+
+        adaptor = cypher_session_adaptor(type, url_or_path, options.merge(wrap_level: :proc))
+        session = Neo4j::Core::CypherSession.new(adaptor)
+        wait_and_retry(session) if wait_for_connection
+        session
+      end
+
+      protected
+
+      def wait_and_retry(session)
+        Timeout.timeout(60) do
+          begin
+            session.constraints
+          rescue Neo4j::Core::CypherSession::ConnectionFailedError
+            sleep(1)
+            retry
+          end
+        end
+      rescue Timeout::Error
+        raise Timeout::Error, 'Timeout while waiting for connection to neo4j database'
+      end
+    end
+  end
+end
