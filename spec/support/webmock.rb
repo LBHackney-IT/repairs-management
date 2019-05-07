af0fb404d15dfab02666d5086e18199d5ecb6616
Celia Collins <celiadcollins@gmail.com>
2019-05-07 14:07:51 +0100

    Change from chromedriver-helper to webdrivers
    Chromedriver-helper is out of support from April 2019, they suggest using webdrivers instead.

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,7 +1,16 @@
 require 'webmock/rspec'
+
+allowed_sites = [
+  "https://chromedriver.storage.googleapis.com",
+  "https://github.com/mozilla/geckodriver/releases",
+  "https://selenium-release.storage.googleapis.com",
+  "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver",
+  %{neo4j_db_test}
+]
+
 WebMock.disable_net_connect!({
   allow_localhost: true,
-  allow: %{neo4j_db_test}
+  allow: allowed_sites
 })
 
 WebMock::Config.instance.query_values_notation = :flat_array
