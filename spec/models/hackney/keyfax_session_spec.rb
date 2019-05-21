8bb4086f46c60621576e9975b04b87f0fa27b2c7
Celia Collins <celiadcollins@gmail.com>
2019-05-21 16:24:16 +0100

    Add test

--- spec/models/hackney/keyfax_session_spec.rb
+++ spec/models/hackney/keyfax_session_spec.rb
@@ -14,7 +14,7 @@ describe Hackney::KeyfaxSession do
     }
   end
 
-  def stub_keyfax_session_create(current_page_url: 'https://repairs-hub.hackney.gov.uk/properties/00004769/repair_requests/new', status: 200,
+  def stub_keyfax_get_startup_url(current_page_url: 'https://repairs-hub.hackney.gov.uk/properties/00004769/repair_requests/new', status: 200,
                                                     body: keyfax_url_response_body)
     stub_request(:get, "#{ ENV['HACKNEY_REPAIRS_API_BASE_URL'] }/v1/keyfax/get_startup_url/?returnurl=#{current_page_url}")
       .to_return(status: status, body: body.to_json)
@@ -22,7 +22,7 @@ describe Hackney::KeyfaxSession do
 
   describe '.create' do
     it 'gets a url' do
-      stub_keyfax_session_create
+      stub_keyfax_get_startup_url
 
       keyfax_instance = Hackney::KeyfaxSession.create(current_page_url: "https://repairs-hub.hackney.gov.uk/properties/00004769/repair_requests/new")
 
