abb2db97fd8004a8f712c9bd2a4cabe0d751eeb6
space pudim <rogi@skylittlesystem.org>
2019-06-04 18:04:16 +0100

    repair_requests#created add new repair link.

--- spec/features/repair_request_spec.rb
+++ spec/features/repair_request_spec.rb
@@ -279,6 +279,7 @@ RSpec.describe 'Repair request' do
       expect(page).to have_text("Repair work order created")
       expect(page).to have_text("Work order number\n01552718")
       expect(page).to have_link("open DRS", href: drs_url)
+      expect(page).to have_link("Raise new repair on 1 Madeup Road", href: new_property_repair_request_path("00000666"))
       expect(page).to have_link("Back to 1 Madeup Road", href: property_path("00000666"))
       expect(page).to have_link("Start a new search", href: root_path)
     end
