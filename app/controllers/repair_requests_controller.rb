692d1203532c37525f73248ec9688ad207d005c3
space pudim <rogi@skylittlesystem.org>
2019-06-06 16:41:34 +0100

    Issue DLO work orders on repair_requests#create.

--- app/controllers/repair_requests_controller.rb
+++ app/controllers/repair_requests_controller.rb
@@ -29,9 +29,10 @@ class RepairRequestsController < ApplicationController
   def create
     @repair_request = Hackney::RepairRequest.new(repair_request_params)
     @repair_request.property_reference = @property.reference
-    @repair_request.created_by_email = session[:current_user]["email"]
+    @repair_request.created_by_email = current_user_email
     respond_to do |format|
       if @repair_request.save
+        issue_dlo_work_orders
         format.html { render :created }
       else
         @cautionary_contacts = Hackney::CautionaryContact.find_by_property_reference(@property.reference)
@@ -43,6 +44,33 @@ class RepairRequestsController < ApplicationController
   end
 
   private
+
+  def issue_dlo_work_orders
+    n_errors = 0
+    @repair_request.work_orders.each do |work_order|
+      if work_order.is_dlo?
+        begin
+          HackneyAPI::RepairsClient.new.post_work_order_issue(
+            work_order.reference,
+            created_by_email: current_user_email
+          )
+        rescue HackneyAPI::RepairsClient::ApiError => e
+          n_errors += 1
+        end
+      end
+    end
+
+    if 0 < n_errors
+      flash.now[:error] ||= []
+      flash.now[:error] << "There was an error issuing the work order and it
+      may not appear in DRS. This can still be done through UH. If the problem
+      persists, please contact the development team."
+    end
+  end
+
+  def current_user_email
+    session[:current_user]["email"]
+  end
 
   def keyfax_session_started?
     params["guid"].present?
