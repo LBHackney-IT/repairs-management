03a01e11d247ccd4ab76c959c2e02509ab2c8cb0
space pudim <rogi@skylittlesystem.org>
2019-06-10 15:29:47 +0100

    Track user (anonymized), controller and action.
    Google analytics.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  # Salt for generating anonymized user ids
+  ANONYMIZED_USER_ID_SALT = ENV["ANONYMIZED_USER_ID_SALT"]
+
+  ANONYMIZED_USER_ID_SALT.present? or raise "No ANONYMIZED_USER_ID_SALT supplied."
+  10 < ANONYMIZED_USER_ID_SALT.size or raise "ANONYMIZED_USER_ID_SALT must be at least 10 characters long."
+
   def user_signed_in?
     session.key?(:current_user)
   end
@@ -21,5 +27,13 @@ module ApplicationHelper
       callback_url = url_encode(logout_url)
       "https://login.microsoftonline.com/#{application_id}/oauth2/logout?post_logout_redirect_uri=#{callback_url}"
     end
+  end
+
+  def current_user_email
+    session[:current_user]["email"]
+  end
+
+  def anonymized_current_user_id
+    Digest::SHA256.hexdigest(ANONYMIZED_USER_ID_SALT + current_user_email)
   end
 end
